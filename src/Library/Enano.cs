313530bd31511574d74f26880625fbf9962f1462
Valentina Hernández Rovere <valentinahernandezrovere@MacBook-Air-de-Valentina.local>
2026-09-09 10:19:53 -0300

    cambiecitos

---
+++ src/Library/Enano.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+public class Enano
+{
+    public string Nombre {get; set;}
+    public int VidaActual{get; set;}
+    public List<> Elementos {get; set;}
+    public int Fuerza {get; set;}
+    public int Resistencia {get; set;}
+    public Enano(string nombre, int vidaActual, int fuerza, int resistencia)
+    {
+        Nombre=nombre;
+        
+    }
+
+
+}
