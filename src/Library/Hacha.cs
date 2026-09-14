b9957c8eec996c1043144826215b6f7ff7e114fe
federica <pepylongueiraviera@gmail.com>
2026-09-14 10:37:48 -0300

    cosas

---
+++ src/Library/Hacha.cs
@@ -0,0 +1,50 @@
+using System;
+public class Hacha
+{
+    private String nombre;
+    private int ataque;
+    private int defensa;
+
+  public Hacha(int ataque,int defensa, String nombre)
+    {
+        Ataque=ataque;
+        Defensa=defensa;
+        nombre=nombre;
+    }  
+public String Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+        set
+        {
+            nombre=value;
+        }
+    }
+    public int Defensa
+    {
+        get
+        {
+            return nombre;
+        }
+        set
+        {
+            nombre=value;
+        }
+    }
+public String Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+        set
+        {
+            nombre=value;
+        }
+    }
+
+
+
+}
