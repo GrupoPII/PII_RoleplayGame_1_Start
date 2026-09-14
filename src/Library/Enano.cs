3a5c25b9a48a61d6d4b6dd8c18d832db3252b7cb
Valentina Hernández Rovere <valentinahernandezrovere@MacBook-Air-de-Valentina.local>
2026-09-14 10:38:41 -0300

    creaciones

--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -5,14 +5,23 @@ public class Enano
 {
     public string Nombre {get; set;}
     public int VidaActual{get; set;}
-    public List<> Elementos {get; set;}
+    public List<dynamic> Elementos {get; set;}
     public int Fuerza {get; set;}
     public int Resistencia {get; set;}
     public Enano(string nombre, int vidaActual, int fuerza, int resistencia)
     {
         Nombre=nombre;
-        
+        VidaActual=vidaActual;
+        Fuerza=fuerza;
+        Resistencia=resistencia;
+        Elementos= new List<dynamic>();
     }
-
-
+    public void AgregarElemento(Escudo escudo)
+    {
+        Elementos.Add(escudo);
+    }
+    public void AgregarElemento(Martillo martillo)
+    {
+        Elementos.Add(martillo);
+    }
 }
---
+++ src/Library/IPersonaje.cs
@@ -0,0 +1,7 @@
+using System.Diagnostics.Contracts;
+
+public interface Ipersonaje
+{
+    public string Nombre {get; set}
+    public int Vida {get; set;}
+}
