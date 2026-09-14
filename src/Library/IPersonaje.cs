b1c01b4ff051be659c95d9a0203957f9ae98cf02
Valentina Hernández Rovere <valentinahernandezrovere@MacBook-Air-de-Valentina.local>
2026-09-14 10:47:27 -0300

    IPersonaje

--- src/Library/IPersonaje.cs
+++ src/Library/IPersonaje.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
 
 public interface Ipersonaje
 {
     public string Nombre {get; set}
     public int Vida {get; set;}
+    public int Fuerza {get; set; }
+    public List<IElemento> Elementos {get; set}
+    public void AgregarElemento( IElemento elemento);
+    public void QuitarElemento (IElemento elemento);
+    public void CambiarElemento (IElemento elemento);
+    public int AtaqueTota();
+    public int DefensaTotal();
+    public void Atacar(IPersonaje personaje);
+    public void Curar();
+
+
 }
