c1e250b7924b47ce216d457cf0f76decb39cbdc4
Valentina <valesosarodrigu@gmail.com>
2026-09-14 10:37:16 -0300

    Hola

--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
 
-public class Enano
+public class Enano //holasoy vale haciendo un cambio
 {
     public string Nombre {get; set;}
     public int VidaActual{get; set;}
