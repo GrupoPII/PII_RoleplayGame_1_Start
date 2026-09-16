047eb3e315b0c9b36324eafc2f6d2da5f79ee09b
federica <pepylongueiraviera@gmail.com>
2026-09-16 09:09:39 -0300

    hola

--- src/Library/IElemento.cs
+++ src/Library/IElemento.cs
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-public class IElemento
-{
-    public string nombre;
-    public int ataque;
-    public int defensa;
-
-    public IElemento(string nombre, int ataque, int defensa)
-    {
-      this.nombre=nombre;
-      this.ataque=ataque;
-      this.defensa=defensa;  
-    }
-
-    public int valorAtaque()
-    {
-        return ataque;
-    }
-    public int valorDefensa()
-    {
-        return defensa;
-    }
-=======
 public interface IElemento
 {
     string Name {get; set;}
